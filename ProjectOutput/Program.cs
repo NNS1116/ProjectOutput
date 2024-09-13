f16db764904c8457c4ae688ac3f85ff86aa2ef67
Nahlan <nahlan-nahla@outlook.com>
2024-09-13 03:42:17 -0400

    Initial commit

---
+++ ProjectOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PersonLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student();
+        string phoneNumber = student.GetCellPhoneNumber();
+        Console.WriteLine($"Student's phone number is: {phoneNumber}");
+    }
+}
